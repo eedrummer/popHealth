58340890cbd5ec40d818b44f5a8718f69480b2dc
Andy Gregorowicz <andy@mitre.org>
2011-02-25 10:50:18 -0500

    Turning on thread safety and creating a worker pool for the web app

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module PopHealth
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.allow_concurrency = true
   end
 end
